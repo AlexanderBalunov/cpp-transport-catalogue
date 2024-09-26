73f537e11db66fc2cc945a9651017d39b214d1c7
Alexander <142852639+AlexanderBalunov@users.noreply.github.com>
2024-09-26 07:04:39 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,1 +1,42 @@
-// место для вашего кода
+#include "stat_reader.h"
+
+#include <iomanip>
+#include <set>
+
+namespace transport {
+
+void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request, std::ostream& output) {
+    size_t command_begin_pos = request.find_first_not_of(' ');
+    auto command_name = request.substr(command_begin_pos, request.find(' ', command_begin_pos));
+    
+    size_t id_name_begin_pos = request.find_first_not_of(' ', request.find(' ', command_begin_pos));
+    auto id_name = request.substr(id_name_begin_pos, request.find_last_not_of(' ') - id_name_begin_pos + 1);
+        
+    if (command_name == "Bus") { 
+        const RouteInfo result = tansport_catalogue.GetRouteInfo(std::string(id_name));
+        if (result.number_of_stops == 0) {
+            output << "Bus " << id_name << ": not found" << std::endl;
+            return;
+        }
+        output << "Bus " << id_name << ": " << result.number_of_stops << " stops on route, "
+                                              << result.number_of_unique_stops << " unique stops, "
+                                              << std::setprecision(6) << result.length << " route length" << std::endl;
+    } else if (command_name == "Stop") {
+        const auto result = tansport_catalogue.GetRoutesThroughStop(std::string(id_name));
+        if (!result) {
+            output << "Stop " << id_name << ": not found" << std::endl;
+        } else if (result->empty()) {
+            output << "Stop " << id_name << ": no buses" << std::endl;
+        } else {
+            output << "Stop " << id_name << ": buses";
+            for (const std::string_view& stop : *result) {
+                output << " " << stop;
+            }
+            output << std::endl;
+        }        
+    } else {
+        throw std::invalid_argument("Unknown request in func ParseAndPrintStat.");
+    }
+}
+    
+}
