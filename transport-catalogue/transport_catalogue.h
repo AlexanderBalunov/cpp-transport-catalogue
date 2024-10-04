e42b3d14382caab0f4cd0a69dc8dc1115f6fce4e
Alexander <142852639+AlexanderBalunov@users.noreply.github.com>
2024-10-04 21:37:15 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <deque>
-#include <optional>
-#include <set>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #include "geo.h"
@@ -31,19 +30,19 @@ class TransportCatalogue {
 public:
     void AddStop(const std::string& stop_name, const geo::Coordinates& stop_coorditanes);    
     void AddRoute(const std::string& route_name, const std::vector<std::string>& route_stops);   
-    const Stop& GetStop(const std::string& stop_name) const;
-    const Route& GetRoute(const std::string& route_name) const;
-    RouteInfo GetRouteInfo(const std::string& route_name) const; 
-    std::optional<std::set<std::string_view>> GetRoutesThroughStop(const std::string& stop_name) const;
+    const Stop* GetStop(const std::string_view& stop_name) const;
+    const Route* GetRoute(const std::string_view& route_name) const;
+    RouteInfo GetRouteInfo(const std::string_view& route_name) const; 
+    std::unordered_set<std::string_view> GetRoutesThroughStop(const std::string& stop_name) const;
      
 private:
-    double СalculateRouteLength(const std::vector<std::string> stops) const;
+    double CalculateRouteLength(const Route& route) const;
     
     std::deque<Stop> stops_;
     std::deque<Route> routes_;
     std::unordered_map<std::string_view, const Stop*> stop_info_by_stop_name_;
     std::unordered_map<std::string_view, const Route*> route_info_by_route_name_;
-    std::unordered_map<std::string_view, std::set<std::string_view>> routes_through_stop_by_stop_name_;
+    std::unordered_map<std::string_view, std::unordered_set<std::string_view>> routes_through_stop_by_stop_name_;
 };
     
 }
