0d1b5dac6522a2509d37da6e1dedff900f2e479c
Alexander <142852639+AlexanderBalunov@users.noreply.github.com>
2024-10-08 21:46:50 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -3,6 +3,11 @@
 #include <algorithm>
 
 namespace transport {
+    
+size_t NearbyStopsHasher::operator()(std::pair<const Stop*, const Stop*> nearby_stops) const {
+    return static_cast<size_t>(37*std::hash<const void*>()(nearby_stops.first) + 
+                                  std::hash<const void*>()(nearby_stops.second));
+}
 
 void TransportCatalogue::AddStop(const std::string& stop_name, const geo::Coordinates& stop_coordinates) {
     stops_.push_back({stop_name, stop_coordinates});
@@ -10,6 +15,19 @@ void TransportCatalogue::AddStop(const std::string& stop_name, const geo::Coordi
     routes_through_stop_by_stop_name_[stops_.back().name];
 }
 
+void TransportCatalogue::AddDistance(std::string_view stop_from, const std::unordered_map<std::string_view, int>& distances) {
+    for (auto [stop_to, m] : distances) {
+        distances_between_stops_[{GetStop(stop_from), GetStop(stop_to)}] = m;
+        if (!distances_between_stops_.count({GetStop(stop_to), GetStop(stop_from)})) {
+            distances_between_stops_[{GetStop(stop_to), GetStop(stop_from)}] = m;
+        }
+    }
+}
+    
+int TransportCatalogue::GetDistance(std::string_view stop_from, std::string_view stop_to) const {
+    return distances_between_stops_.at({GetStop(stop_from), GetStop(stop_to)});    
+}
+    
 void TransportCatalogue::AddRoute(const std::string& route_name, const std::vector<std::string>& route_stops) {
     routes_.push_back({route_name, route_stops});
     route_info_by_route_name_[routes_.back().name] = &routes_.back();       
@@ -34,11 +52,14 @@ const Route* TransportCatalogue::GetRoute(std::string_view route_name) const {
 
 RouteInfo TransportCatalogue::GetRouteInfo(std::string_view route_name) const {
     if (!route_info_by_route_name_.count(route_name)) {
-        return {0, 0, 0.0};
+        return {0, 0, 0, 0.0};
     }
+    int real_route_length = CalculateRealRouteLength(*route_info_by_route_name_.at(route_name));
     return {GetRoute(route_name)->stops.size(),
-           std::unordered_set(GetRoute(route_name)->stops.begin(), GetRoute(route_name)->stops.end()).size(), 
-           CalculateRouteLength(*route_info_by_route_name_.at(route_name))};
+           std::unordered_set(GetRoute(route_name)->stops.begin(), 
+                              GetRoute(route_name)->stops.end()).size(),
+                              real_route_length,
+                              real_route_length / CalculateGeoRouteLength(*route_info_by_route_name_.at(route_name))};
 }
 
 std::unordered_set<std::string_view> TransportCatalogue::GetRoutesThroughStop(std::string_view stop_name) const {
@@ -48,7 +69,15 @@ std::unordered_set<std::string_view> TransportCatalogue::GetRoutesThroughStop(st
     return routes_through_stop_by_stop_name_.at(stop_name);
 }
 
-double TransportCatalogue::CalculateRouteLength(const Route& route) const {
+int TransportCatalogue::CalculateRealRouteLength(const Route& route) const {
+    int route_length = 0;
+    for (size_t i = 0; i < route.stops.size() - 1; ++i) {
+        route_length += GetDistance(route.stops[i], route.stops[i+1]);
+    }
+    return route_length;
+}
+    
+double TransportCatalogue::CalculateGeoRouteLength(const Route& route) const {
     std::vector<geo::Coordinates> stops_coordinates;
     stops_coordinates.reserve(route.stops.size());
     for (const std::string& stop_name : route.stops) {
