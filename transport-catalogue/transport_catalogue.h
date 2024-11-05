e67c5b9651c900316ab03aae40f7549298607d8b
Alexander <142852639+AlexanderBalunov@users.noreply.github.com>
2024-11-05 08:11:02 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,51 +1,52 @@
 #pragma once
 
+#include "domain.h"
+#include "geo.h"
+
+#include <deque>
+#pragma once
+
+#include "domain.h"
+#include "geo.h"
+
 #include <deque>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
-#include "geo.h"
-
-namespace transport {
-
-struct Stop {
-    std::string name;
-    geo::Coordinates coordinates;
-};
-
-struct Route {
-    std::string name;
-    std::vector<std::string> stops;
-};
-
-struct RouteInfo {
-    size_t number_of_stops;
-    size_t number_of_unique_stops;
-    int length;
-    double curvature;
-};
-
-class NearbyStopsHasher {
-public:
-    size_t operator()(std::pair<const Stop*, const Stop*> nearby_stops) const;
-};    
+namespace transport {   
     
 class TransportCatalogue {
-public:
+public: 
     void AddStop(const std::string& stop_name, const geo::Coordinates& stop_coorditanes);
     void AddDistance(std::string_view stop_from, std::string_view stop_to, int distance);
-    int GetDistance(std::string_view stop_from, std::string_view stop_to) const;
-    void AddRoute(const std::string& route_name, const std::vector<std::string>& route_stops);  
+    void AddRoute(const std::string& route_name, const std::vector<std::string>& route_stops, bool is_roundtrip);  
     const Stop* GetStop(std::string_view stop_name) const;
     const Route* GetRoute(std::string_view route_name) const;
+    int GetDistance(std::string_view stop_from, std::string_view stop_to) const;
+    
+    struct RouteInfo {
+        int number_of_stops;
+        int number_of_unique_stops;
+        int length;
+        double curvature;
+    };
+    
     RouteInfo GetRouteInfo(std::string_view route_name) const; 
-    std::unordered_set<std::string_view> GetRoutesThroughStop(std::string_view stop_name) const;
+    const std::unordered_set<std::string_view>* GetRoutesThroughStop(std::string_view stop_name) const;
+    
+    const std::unordered_map<std::string_view, const Route*>& GetAllRoutes() const;
+    const std::unordered_map<std::string_view, const Stop*>& GetAllStops() const;
      
 private:
     double CalculateGeoRouteLength(const Route& route) const;
     int CalculateRealRouteLength(const Route& route) const;
+    
+    class NearbyStopsHasher {
+    public:
+        size_t operator()(std::pair<const Stop*, const Stop*> nearby_stops) const;
+    }; 
     
     std::deque<Stop> stops_;
     std::deque<Route> routes_;
@@ -55,4 +56,4 @@ private:
     std::unordered_map<std::pair<const Stop*, const Stop*>, int, NearbyStopsHasher> distances_between_stops_;    
 };
     
-}
+} // namespace transport
